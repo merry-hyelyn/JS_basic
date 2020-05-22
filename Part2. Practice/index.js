d2c4049b6e2a433551734b93568a06aec6952dad
merryn <h1335@naver.com>
2020-05-22 16:44:36 +0900

    first commit

---
+++ Part2. Practice/index.js
@@ -0,0 +1,11 @@
+const title = document.getElementById("title")
+
+const CLICKED_CLASS = "clicked"
+
+function handleClick() {
+    
+}
+function init() {
+    title.addEventListener("click", handleClick)
+}
+init()
---
+++ Part3. Make App/clock.js
@@ -0,0 +1,22 @@
+const clockContainer = document.querySelector(".js-clock"),
+    clockTitle = clockContainer.querySelector("h1")
+
+function getTime() {
+    const date = new Date()
+    const minutes = date.getMinutes()
+    const hours = date.getHours()
+    const seconds = date.getSeconds()
+
+    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
+        minutes < 10 ? `0${minutes}` : minutes
+    }:${seconds < 10 ? `0${seconds}` : seconds}`
+}
+function init() {
+    getTime()
+    setInterval(getTime, 1000)
+}
+init()
+
+// setInterval(function, seconds)
+// function 호출 시 function() X -> function
+// function() -> function 즉시 호출, setInterval보다 먼저 실행

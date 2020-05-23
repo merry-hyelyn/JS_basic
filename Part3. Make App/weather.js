82125d61156ac68ad31b56900db00a58d13c5fcd
merryn <h1335@naver.com>
2020-05-23 17:08:13 +0900

    Add weather API.. awesome..!

--- Part3. Make App/weather.js
+++ Part3. Make App/weather.js
@@ -1,5 +1,21 @@
+const weather = document.querySelector(".js-weather")
 const COORDS = "coords"
+const API_KEY = "785a2b7e8615b11ddd7a89d3b0d56a1d"
 
+function getWeather(lat, lng) {
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=imperial`
+    )
+        .then(function (response) {
+            return response.json()
+            // then을 이용하여 request한 데이터(fetch api)가 모두 넘어온 후에 함수 실행
+        })
+        .then(function (json) {
+            const temperature = json.main.temp
+            const place = json.name
+            weather.innerText = `${temperature} @ ${place}`
+        })
+}
 function saveCoords(coordsObj) {
     localStorage.setItem(COORDS, JSON.stringify(coordsObj))
 }
@@ -12,6 +28,7 @@ function handleGeoSuccess(position) {
         longitude, // longitude = longitude
     }
     saveCoords(coordsObj)
+    getWeather(latitude, longitude)
 }
 
 function handleGeoError() {
@@ -25,10 +42,13 @@ function askForCoords() {
 // 현재 위치 불러오기
 function loadCoords() {
     const loadedCoords = localStorage.getItem(COORDS)
+    // 현재 위치 loaderCoords에 저장
+
     if (loadedCoords === null) {
         askForCoords()
     } else {
-        // getWeather
+        const parseCoords = JSON.parse(loadedCoords)
+        getWeather(parseCoords.latitude, parseCoords.longitude)
     }
 }
 
